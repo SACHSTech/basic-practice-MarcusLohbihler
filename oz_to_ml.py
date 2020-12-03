4341d4334fe12ca3bf7bcb2efa1f53680536441a
Marcus Lohbihler <marcus.lohbihler23@ycdsbk12.ca>
2020-12-03 18:44:48 +0000

    Added oz_to_ml.py and cake_jog.py

---
+++ oz_to_ml.py
@@ -0,0 +1,4 @@
+oz = float(input("Enter the amount of liquid in ounces "))
+serve = float(input("Enter the number of people to serve"))
+ml = (oz * serve)*29.574
+print ("You will need " + str(ml) + " ml")
